afcd09fe7d8035f7f984aa38b55dad83accc0ff8
JSander72 <jamesearlsanders2@outlook.com>
2026-02-11 17:52:27 -0500

    fix: remove deprecated comment in postcss.config.js and ensure production environment setting

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
     autoprefixer: {
-      // Suppress color-adjust deprecation warning
       overrideBrowserslist: ['>0.2%', 'not dead', 'not op_mini all'],
+      env: 'production',
     },
   },
 };
